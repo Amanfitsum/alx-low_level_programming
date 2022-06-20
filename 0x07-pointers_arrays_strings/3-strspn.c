ef5978e6c6c1cf2bcb1a480f0bfefd3954fd5425
Amanuel Gebreyesus <amanuealfitsum1@gmail.com>
2022-06-20 03:24:57 -0700

    my commit message

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -23,13 +23,13 @@ while (str1[i] != '\0') /*Declaring WHILE *s */
 j = 0;
 while (str2[j] != '\0') /*Declaring WHILE *accept*/
 {
-if (str2[j] == str[i]) /*Evaluate condition*/
+if (str2[j] == str1[i]) /*Evaluate condition*/
 {
 count++; /*count number*/
 break;
 }
 
-j++; /*add j+1*/
+j++;   /*add j+1*/
 }
 
 if (s[i] != accept[j]) /*If aren't equals*/
