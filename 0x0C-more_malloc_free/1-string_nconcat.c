63bc47e48b3c890c77d9e2a37dde6924b8a41bcc
Amanuel Gebreyesus <amanuealfitsum1@gmail.com>
2022-06-30 09:42:39 -0700

    my commit message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;						
 	
-	if (n > ls2)							              n = ls2;
+	if (n > ls2)							             n = ls2;
 	
 	lsout = ls1 + n;
 			
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (sout == NULL)
                 return (NULL);
-									      for (i = 0; i < sout; i++)
+									     for (i = 0; i < sout; i++)
 	        if (i < ls1)								      sout[i] = s1[i];
 	        else
 		        sout[i] = s2[i - ls1];							    
